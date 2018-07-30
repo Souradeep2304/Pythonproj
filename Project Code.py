16ea2f635dea9395cef43c140cde2364889f77fd
Souradeep Banerjee <souradeepbanerjee2304@gmail.com>
2018-07-30 23:52:09 +0530

    Update prof added with some minor tweaks

--- Project Code.py
+++ Project Code.py
@@ -1,9 +1,10 @@
 import mysql.connector
 
 conn= mysql.connector.connect(user='root', password='1234', database='pharmacy')
+
 curs=conn.cursor()
 
-class user:
+class user:   #completed
     def __init__(self):
         uid=None
 
@@ -15,7 +16,7 @@ class user:
 
     def signin(self,uname,password): #DONE
         try:
-            curs.execute("""SELECT password from pharmacy.customer WHERE email=%s""", (uname,))
+            curs.execute("""SELECT password from pharmacy.customer WHERE email=%s""", (uname))
             passw=curs.fetchall()
             if password==passw[0][0]:
                 return True
@@ -37,19 +38,21 @@ class customer(user):
     def previousOrder(self):
         pass
 
-    def updateProfile(self, cid): #DONE    #added uname for condition
+    def updateProfile(self, cid):  # DONE    #added uname for condition
         name = input("Enter your Name:")
         password = input("Enter password:")
         phone = input("Enter phone number:")
         email = input("Enter email address:")
         address = input("Enter address:")
-        curs.execute(""" UPDATE pharmacy set cname=%s, phone=%s, email=%s, address=%s WHERE cid=%s""",(name,phone,email,address,cid))
-        pass
+        curs.execute(""" UPDATE pharmacy.customer set cname=%s, password=%s, phone=%s, email=%s, address=%s WHERE cid=%s""",
+        (name, password, phone, email, address, cid))
+        print(curs.rowcount,"record updated successfully!")
+        conn.commit()
 
     def getDiscount(self):
         pass
 
-    def fetchDetails(self,name,password,phone,email,address):
+    def fetchDetails(self,name,password,phone,email,address): # DONE
         self.cust_details['Name'] = name
         self.cust_details['Password'] = password
         self.cust_details['Phone'] = phone
@@ -62,50 +65,55 @@ class customer(user):
 #Program Menu
 print("1.Press 1 for Customer")
 print("2.Press 2 for Admin")
-choice1=int(input("Enter Choice:"))
-if choice1==1:
+choice1 = int(input("Enter Choice:"))
+if choice1 == 1:
     print("1.Press 1 for Signup")
     print("2.Press 2 for signin")
-    choice2=int(input("Enter choice:"))
-    if choice2==1:
+    choice2 = int(input("Enter choice:"))
+    if choice2 == 1:
         name = input("Enter your Name:")
         password = input("Enter password:")
         phone = input("Enter phone number:")
         email = input("Enter email address:")
         address = input("Enter address:")
         obj = customer()
-        obj.fetchDetails(name,password,phone,email,address)
-    elif choice2==2:
-        tf=False
-        while tf!=True:
-            uname=input("Enter Username(Email):")
-            password=input("Enter Password:")
-            obj=customer()
-            tf=obj.signin(uname,password)
-            if tf==True:
+        obj.fetchDetails(name, password, phone, email, address)
+    elif choice2 == 2:
+        tf = False
+        while tf != True:
+            uname = input("Enter Username(Email):")
+            password = input("Enter Password:")
+            obj = customer()
+            tf = obj.signin(uname, password)
+            if tf == True:
                 print("Login Successful!")
-                curs.execute(""" SELECT cid,cname, address, phone, email from pharmacy.customer WHERE email=%s""",(uname,))
-                r=curs.fetchall()
-                print("WELCOME "+r[0][1]+"!")
-                
-                tg=False
-                while tg!=True:
+                curs.execute(""" SELECT cid,cname, address, phone, email from pharmacy.customer WHERE email=%s""",
+                             (uname,))
+                r = curs.fetchall()
+                cid = r[0][0]
+                print("WELCOME " + r[0][1] + "!")
+
+                tg = False
+                while tg != True:
                     print("1.Press 1 to view profile details.")
                     print("2.Press 2 to update profile details.")
                     print("3.Press 3 to order new items.")
                     print("4.Press 4 to see previous orders.")
                     print("5.Press 5 to log Out.")
-                    choice3=int(input("Enter choice:"))
-                    if choice3==1:
-                        print("Customer Name:",r[0][1])
-                        print("Customer ID:",r[0][0])
-                        print("Customer address:",r[0][2])
-                        print("Customer Email:",r[0][4])
-                        print("Customer phone:",r[0][3])
-                    if choice3==5:
-                        tg=True
-                    
+                    choice3 = int(input("Enter choice:"))
+                    if choice3 == 1:
+                        print("Customer Name:", r[0][1])
+                        print("Customer ID:", r[0][0])
+                        print("Customer address:", r[0][2])
+                        print("Customer Email:", r[0][4])
+                        print("Customer phone:", r[0][3])
+                    elif choice3 == 2:
+                        obj.updateProfile(cid)
+                    elif choice3 == 3:
+                        pass
+                    elif choice4 == 4:
+                        pass
+                    elif choice3 == 5:
+                        tg = True
             else:
                 print("Login Unsuccessful!")
-
-#COMPLETED TILL SIGNIN SIGNUP
